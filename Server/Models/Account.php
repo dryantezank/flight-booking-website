a54b7798b156e64f34d819fc357771829ac26e21
dryantezank <lamhuynhphuagu@gmail.com>
2023-12-14 14:47:13 +0700

    Add checking username & password of user account method to Account.php

--- Server/Models/Account.php
+++ Server/Models/Account.php
@@ -18,9 +18,42 @@ class Account {
             $cmd = $dbconn->prepare($sql);
             $cmd->execute();
             $result = $cmd->fetchAll();
+            Database::close_connect();
             return $result;
         } catch(PDOException $e) {
+            echo "<p>Lỗi truy vấn: {$e->getMessage()}</p>";
+            exit();
+        }
+    }
 
+    // Lấy account theo id
+    public function getAccountById(int $id) : mixed {
+        $dbconn = Database::connect();
+        try {
+            $sql = "SELECT * FROM account WHERE id=:id";
+            $cmd = $dbconn->prepare($sql);
+            $cmd->bindValue(":id", $id);
+            $result = $cmd->fetch();
+            Database::close_connect();
+            return $result;
+        } catch(PDOException $e) {
+            echo "<p>Lỗi truy vấn: {$e->getMessage()}</p>";
+            exit();
+        }
+    }
+
+    public function checkAccount(string $accountname, string $password) : bool {
+        $dbconn = Database::connect();
+        try {
+            $sql = "SELECT * FROM account WHERE accountname=:accountname,password=:password";
+            $cmd = $dbconn->prepare($sql);
+            $cmd->bindValue(":accountname", $accountname);
+            $cmd->bindValue(":password", $password);
+            $result = $cmd->fetch();
+            return !empty($result);
+        } catch (PDOException $e) {
+            echo "<p>Lỗi truy vấn: {$e->getMessage()}</p>";
+            exit();
         }
     }
 }
